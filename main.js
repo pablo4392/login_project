e441c3a2d5cee3b9297b90c38aa31b12b397c2ee
Pablo C Ramirez <carp4392@gmail.com>
2021-10-13 00:59:14 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// SHOW PASSWORD
+const user = document.getElementById('user')
+const password = document.getElementById('password')
+const eyeButton = document.querySelector(".far")
+let isPass = true;
+
+const togglePass = () => {
+    if(isPass) {
+        password.setAttribute("type", "text");
+        eyeButton.classList.remove('fa-eye-slash');
+        eyeButton.classList.add('fa-eye');
+        isPass = false;
+    } else {
+        password.setAttribute("type", "password");
+        eyeButton.classList.remove('fa-eye');
+        eyeButton.classList.add('fa-eye-slash');
+        isPass = true;
+    }
+}
+
+const show = () => {
+    alert(user.value);
+    alert(password.value)
+}
