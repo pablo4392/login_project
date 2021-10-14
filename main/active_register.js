d5253b104c6699b4286329d3623da6c1b658fc80
Pablo C Ramirez <carp4392@gmail.com>
2021-10-13 20:41:52 -0500

    new comit

--- main/active_register.js
+++ main/active_register.js
@@ -1,20 +1,18 @@
 const loginForm = document.getElementById('loginForm');
 const registerForm = document.getElementById('registerForm');
-const registerButton = document.getElementById('registerButton');
-const cancelButton = document.getElementById('cancelButton');
+// const registerButton = document.getElementById('registerButton');
+// const cancelButton = document.getElementById('cancelButton');
 
-registerButton.addEventListener('click', (event) => {
-    event.preventDefault();
+const registerActivation= () => {
     registerForm.classList.add('formulary');
     registerForm.classList.remove('display-none');
     loginForm.classList.remove('formulary')
     loginForm.classList.add('display-none');    
-});
+};
 
-cancelButton.addEventListener('click', (event) => {
-    event.preventDefault();
+const cancelFunction = () => {
     registerForm.classList.add('display-none');
     registerForm.classList.remove('formulary');
     loginForm.classList.add('formulary');
     loginForm.classList.remove('display-none');
-});
+};
---
+++ main/validation.js
@@ -0,0 +1,10 @@
+const name = document.getElementById('registerName');
+const lastName = document.getElementById('registerLastName');
+const mail = document.getElementById('registerMail');
+const userName = document.getElementById('registerUsername');
+const password = document.getElementById('registerPassword');
+const confirmPassword = document.getElementById('confirmPassword');
+
+const send = (e) => {
+    e.preventDefault()
+}
